501c43915662d37b1943596c269f43d6bc1e5cf6
solodod <solodod@cf405-17.cs.wwu.edu>
2025-05-05 14:56:06 -0700

    Worked on creating Menubar, still a lot of work to do for homescreen

--- budget_bridge/src/main/java/groupid/App.java
+++ budget_bridge/src/main/java/groupid/App.java
@@ -5,9 +5,15 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
-
+import javafx.scene.control.Button; 
+import javafx.scene.layout.*; 
+import javafx.event.ActionEvent; 
+import javafx.event.EventHandler; 
+import javafx.scene.control.*; 
+import javafx.stage.Stage; 
+import javafx.scene.control.Alert.AlertType; 
+import java.time.LocalDate; 
 /**
  * JavaFX App
  */
@@ -17,9 +23,34 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        stage.setTitle("BudgetBridge");
+        
+        //Create a menu
+        Menu m = new Menu("MENU");
+
+        // create menu items
+        MenuItem m1 = new MenuItem("TEST-1");
+        MenuItem m2 = new MenuItem("TEST-2");
+        MenuItem m3 = new MenuItem("TEST-3");
+
+        // add menu items
+        m.getItems().add(m1);
+        m.getItems().add(m2);
+        m.getItems().add(m3);
+
+        // create a menu bar
+        MenuBar mb = new MenuBar();
+
+        mb.getMenus().add(m);
+        VBox vb = new VBox(mb);
+
+        //                loadFXML("primary")
+        scene = new Scene(loadFXML("primary"), 1920, 1080);
         stage.setScene(scene);
         stage.show();
+
+
+        
     }
 
     static void setRoot(String fxml) throws IOException {
